dd1c0a07358b1f1a5ebf7f659fb3c0e416082876
Bruno Santos <bms.santos@outlook.pt>
2025-04-10 12:19:11 +0100

    Hotfix: Weird behaviour on scroll on mobile devices

--- app/page.js
+++ app/page.js
@@ -13,6 +13,7 @@ export default function Home() {
   
       if (!entry.isIntersecting) {
         setShowWelcome(false);
+        document.getElementById('header')?.scrollIntoView({});
         document.body.style.overflow = 'auto';
       }
     }, {
