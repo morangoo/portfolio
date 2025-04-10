90ec43eaa4163ad5aea1ef13382a2c2d3382c021
Bruno Santos <bms.santos@outlook.pt>
2025-04-10 13:04:18 +0100

    Hotfix: Weird behaviour on scroll on mobile devices (This might be it!!!)

--- app/page.js
+++ app/page.js
@@ -6,6 +6,8 @@ import Header from "@/app/components/header";
 export default function Home() {
   
   const [showWelcome, setShowWelcome] = useState(true);
+  const [headerLoaded, setHeaderLoaded] = useState(false);
+  const [contentLoaded, setContentLoaded] = useState(false);
 
   useEffect(() => {
     const observer = new IntersectionObserver((entries) => {
@@ -14,11 +16,7 @@ export default function Home() {
       if (!entry.isIntersecting) {
         setShowWelcome(false);
         document.body.style.overflow = 'auto';
-        const elemento = document.getElementById('welcome-container');
-        if (elemento) {
-          const alturaElementoRemovido = elemento.offsetHeight;
-          window.scrollTo(0, window.scrollY - alturaElementoRemovido);
-        }
+        setContentLoaded(true);
       }
     }, {
       root: null,
@@ -37,13 +35,20 @@ export default function Home() {
     };
   }, []);
 
+  useEffect(() => {
+    setHeaderLoaded(true);
+  }, []);
+
   return (
     <div>
       {showWelcome && <Welcome />}
       <Header />
-      <p> teste </p>
-      <p> testeee</p>
-      <p> testeeeee</p>
+      <div id="teste" className="flex h-screen items-center justify-center">
+        <h1>Agora podes dar scroll!</h1>
+      </div>
+      <div id="teste2" className="flex h-screen items-center justify-center">
+        <h1>Agora podes dar scroll!2</h1>
+      </div>
     </div>
   );
 }
