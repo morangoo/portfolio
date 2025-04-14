e05585510974881e8216338e0d3d2d1db2f38cb3
Bruno Santos <bms.santos@outlook.pt>
2025-04-14 17:53:55 +0100

    A little about me :)

--- README.md
+++ README.md
@@ -1,5 +1,4 @@
 # My Portfolio 
 ![](https://i.pinimg.com/736x/49/e5/8e/49e58e06347e5251c93e3ad18f952c56.jpg)
 ### To-Do List
-- Colocar locale
-- Ao passar o rato sobre "MIGUEL" muda para "BRUNO" (utilizar 2 spans com hidden e display block para manter a estrutura da página)
+- Adicionar vídeo na neve no canto superior direito do about me
--- app/components/aboutme.js
+++ app/components/aboutme.js
@@ -1,13 +1,25 @@
 'use client';
 import '@/app/styles/aboutme.css';
 import { useLang } from '@/app/contexts/LangContext';
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import Image from 'next/image';
 
 import BlurText from '@/app/utils/BlurText';
+import FadeContent from '@/app/utils/FadeContent'
 
 const AboutMe = () => {
     const { language, translations } = useLang();
+    const [muted, setMuted] = useState(true);
+    const [animationCompleted, setAnimationCompleted] = useState(false);
+
+    const handleMouseEnter = () => {
+      setMuted(false); 
+    };
+
+    const handleMouseLeave = () => {
+      setMuted(true); 
+    };
+  
 
     const handleAnimationComplete = () => {
     };
@@ -24,8 +36,18 @@ const AboutMe = () => {
         className="about-title"
       />
       </div>
-      <div className="about-container-images">
-      </div>
+      <FadeContent blur={true} duration={1500} easing="ease-out" initialOpacity={0} delay={500}>
+        <div className="about-container-media">
+          <div className="media-container-flex">
+            <img src="/bruges.png" alt="Bruges" className="about-image" />
+            <img src="/ffx.jpg" alt="FFX" className="about-image" />
+          </div>
+          <div className="media-container-flex">
+            <img src="/gym.png" alt="Gym" className="about-image" />
+            <video src="concert.mp4" autoPlay loop muted={muted} className="about-image" id="video" onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}/>
+          </div>
+        </div>
+      </FadeContent>
     </div>
     );
 };
