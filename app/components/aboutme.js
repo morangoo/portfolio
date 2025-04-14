a7d585d0ff5f48109c247234aa0389423238d2f6
Bruno Santos <bms.santos@outlook.pt>
2025-04-14 12:36:18 +0100

    Colors are boring

--- app/components/aboutme.js
+++ app/components/aboutme.js
@@ -2,13 +2,16 @@
 import '@/app/styles/aboutme.css';
 import { useLang } from '@/app/contexts/LangContext';
 import { useState, useEffect } from 'react';
+import Image from 'next/image';
 
 const AboutMe = () => {
     const { language, translations } = useLang();
 
     return (
     <div id="aboutme" className="about-container">
-      <span className="text-2xl text-white">{translations.greeting}</span>
+      <div className="flex">
+        <h1 className="text-5xl">But who's Miguel?</h1>
+      </div>
     </div>
     );
 };
