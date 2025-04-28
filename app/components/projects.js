4a8bb0c94ec60125fa9dd19a8ec7ed46a203bfba
Bruno Santos <bms.santos@outlook.pt>
2025-04-28 10:31:49 +0100

    Mobile users deserve some good animations too.

--- app/components/projects.js
+++ app/components/projects.js
@@ -2,6 +2,7 @@
 import '@/app/styles/projects.css';
 
 import { useLang } from '@/app/contexts/LangContext';
+import { useEffect, useState } from 'react';
 
 import CircularGallery from '@/app/utils/CircularGallery';
 import DecryptedText from '@/app/utils/DecryptedText';
@@ -9,33 +10,48 @@ import AnimatedContent from '@/app/utils/AnimatedContent';
 
 const Projects = () => {
   const { language, translations } = useLang();
+  const [isMobile, setIsMobile] = useState(false);
 
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 768); // Ajusta o limite conforme necessário (768px é um padrão comum).
+    };
+    
+    // Configuração inicial
+    handleResize();
+
+    // Ouvinte de redimensionamento
+    window.addEventListener('resize', handleResize);
+
+    // Limpeza do ouvinte
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
 
   return (
     <div id="projects" className="projects-container">
-    <h1 className="projects-title">
-      <AnimatedContent
-        distance={150}
-        direction={'vertical'}
-        config={{ tension: 80, friction: 20 }}
-        initialOpacity={0.2}
-        animateOpacity
-        scale={1.1}
-        threshold={0.2}
+      <h1 className="projects-title">
+        <AnimatedContent
+          distance={150}
+          direction={'vertical'}
+          config={{ tension: 80, friction: 20 }}
+          initialOpacity={0.2}
+          animateOpacity
+          scale={1.1}
+          threshold={0.2}
         >
-        <DecryptedText
-        text={translations?.projectsScreen?.projects || ''}
-        animateOn="view"
-        revealDirection="start"
-        speed={50}
-        sequential={true}
-        useOriginalCharsOnly={true}
-        />
-      </AnimatedContent>
-    </h1>
+          <DecryptedText
+            text={translations?.projectsScreen?.projects || ''}
+            animateOn="view"
+            revealDirection="start"
+            speed={50}
+            sequential={true}
+            useOriginalCharsOnly={true}
+          />
+        </AnimatedContent>
+      </h1>
       <div className="projects-list-container">
         <CircularGallery 
-          bend={1}
+          bend={isMobile ? 0.25 : 2} // Altera o valor do bend com base no estado
           textColor="#ffffff" 
           borderRadius={0.05}
           items={
--- app/components/techstack.js
+++ app/components/techstack.js
@@ -34,6 +34,7 @@ const TechStack = () => {
             />
         </AnimatedContent>
       </h1>
+      <p>WIP</p>
     </div>
   );
 };
