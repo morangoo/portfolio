b8f9d12bbab798ce1388bd1e9e5e7f4e634c2664
Bruno Santos <bms.santos@outlook.pt>
2025-05-14 16:26:59 +0100

    Fix Toshokan's URL

--- app/components/projects.js
+++ app/components/projects.js
@@ -44,7 +44,7 @@ const Projects = () => {
                 text: "Toshokan",
                 image: "/toshokan.png",
                 id: "toshokan",
-                link: "https://toshokan.vercel.app/",
+                link: "https://toshokan-azure.vercel.app/",
               },
               {
                 text: "Work in Progress (WIP)",
